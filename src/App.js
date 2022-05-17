482041490c499a4fc65de607727b56a716d06606
Kimseonghyoek <cilo1223@naver.com>
2022-05-17 12:08:36 +0900

    update jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import TodoInput from "./components/TodoInput.jsx";
+import './scss/app.scss'
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <TodoInput />
+      <div id="todo-wrap">
+        <h1 className="title">Todo-List</h1>
+        <div id="todo">
+          <TodoInput />
+        </div>
       </div>
     );
   }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,12 +3,16 @@ import '../scss/TodoInput.scss'
 
 class TodoInput extends React.Component {
   render() {
+    const addTodoList = () => {
+      const input = document.getElementById('todoInput')
+    }
     return (
       <div>
         <input 
           placeholder='Edit your work!😁'
+          id='todoInput'
         ></input>
-        <button>➕</button>
+        <button onClick={addTodoList}>➕</button>
       </div>
     )
   }
